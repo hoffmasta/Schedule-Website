5a287cd8b682d52f7d136c5c2e43d09de298f71c
topicalfish <hoffmasta@hotmail.com>
2026-09-17 18:56:36 -0500

    version change

--- Scripts/version.js
+++ Scripts/version.js
@@ -1,7 +1,7 @@
 //Whenever changes to betterDict are made and the version is updated,
 //the below version must match exactly.
 
-version = "1.4";
+version = "1.5";
 
 /*-----------------------------------
 
